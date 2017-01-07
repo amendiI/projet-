785b52b0c258573efa6c61945257ec8b1b8a0b78
amendiI <raphaelmuller@hotmail.fr>
2017-01-07 19:57:16 +0100

    header

---
+++ plop/headers.h
@@ -0,0 +1,34 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include "point.h"
+#include "option.h"
+#include "time.h"
+#include "math.h"
+#include "hitbox.h"
+#include "Point.h"
+#include "terrain.h"
+#include "option.h"
+#include "grilleterrain.h"
+#include "tank.h"
+#include "tir.h"
+#include "tourelle.h"
+#include "caisse.h"
+
+#include <cmath>
+#include <iostream>
+#include <time.h>
+
+#include <QApplication>
+#include <QPushButton>
+#include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+
+
+#endif // HEADERS_H
