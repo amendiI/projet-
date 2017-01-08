95f6478409f63eee5ee95824f273731307d4a6ef
amendiI <raphaelmuller@hotmail.fr>
2017-01-08 14:22:12 +0100

    simplification déplacement

--- plop/caisse.cpp
+++ plop/caisse.cpp
@@ -38,211 +38,157 @@ void Caisse::keyPressEvent(QKeyEvent *event)
         if(angle<0){angle=angle+8;}//pour que le modulo ne calcul pas des negatifs
         if(event->key() ==Qt::Key_D )
         {
-            c->setRotation(rotation()+45);
-            c->tourelle->setRotation(tourelle->rotation()+45);
-            c->setAngle(angle+1);
+            setRotation(rotation()+45);
+            tourelle->setRotation(tourelle->rotation()+45);
+            setAngle(angle+1);
         }
         else if(event->key() ==Qt::Key_Q )
         {
-           c->setRotation(rotation()-45);
-           c->setRotation(tourelle->rotation()-45);
-           c->angle--;
+            setRotation(rotation()-45);
+            tourelle->setRotation(tourelle->rotation()-45);
+            setAngle(angle-1);
         }
-        else if(event->key() ==Qt::Key_Z )
+        else if(event->key() ==Qt::Key_Z ||event->key() ==Qt::Key_S)
         {
+            int xp=0;
+            int yp=0;
             switch (angle%8)
             {
             case 0:
-                setPos( x()+5, y());
-                tourelle->setPos( tourelle->x()+5, tourelle->y());
-                centre.setx(cx+5);
+                xp=5;
                 break;
             case 1:
-                c->setPos( x()+5, y()+5);
-                c->tourelle->setPos( tourelle->x()+5, tourelle->y()+5);
-                c->centre.setx(cx+5);
-                c->centre.sety(cy+5);
+                xp=5;
+                yp=5;
                 break;
             case 2:
-                c->setPos( x(), y()+5);
-                c->tourelle->setPos( tourelle->x(), tourelle->y()+5);
-                c->centre.sety(cy+5);
+                yp=5;
                 break;
             case 3:
-                c->setPos( x()-5, y()+5);
-                c->tourelle->setPos( tourelle->x()-5, tourelle->y()+5);
-                c->centre.setx(cx-5);
-                c->centre.sety(cy+5);
+                xp=-5;
+                yp=5;
                 break;
             case 4:
-                c->setPos( x()-5, y());
-                c->tourelle->setPos( tourelle->x()-5, tourelle->y());
-                c->centre.setx(cx-5);
+                xp=-5;
                 break;
             case 5:
-                c->setPos( x()-5, y()-5);
-                c->tourelle->setPos( tourelle->x()-5, tourelle->y()-5);
-                c->centre.setx(cx-5);
-                c->centre.sety(cy-5);
+                xp=-5;
+                yp=-5;
                 break;
             case 6:
-                c->setPos( x(), y()-5);
-                c->tourelle->setPos( tourelle->x(), tourelle->y()-5);
-                c->centre.sety(cy-5);
+                yp=-5;
                 break;
             case 7:
-                c->setPos( x()+5, y()-5);
-                c->tourelle->setPos( tourelle->x()+5, tourelle->y()-5);
-                c->centre.setx(cx+5);
-                c->centre.sety(cy-5);
+                xp=5;
+                yp=-5;
                 break;
             default:
                 break;
             }
-            pm--;
-        }
-        else if(event->key() ==Qt::Key_S)
-        {
-            switch (angle%8)
+            //pm--;
+
+            if(event->key() ==Qt::Key_S)
             {
-            case 0:
-                c->setPos( x()-5, y());
-                c->tourelle->setPos( tourelle->x()-5, tourelle->y());
-                c->centre.sety(cx-5);
-                break;
-            case 1:
-                c->setPos( x()-5, y()-5);
-                c->tourelle->setPos( tourelle->x()-5, tourelle->y()-5);
-                c->centre.sety(cx-5);
-                c->centre.sety(cy-5);
-                break;
-            case 2:
-                c->setPos( x(), y()-5);
-                c->tourelle->setPos( tourelle->x(), tourelle->y()-5);
-                c->centre.sety(cy-5);
-                break;
-            case 3:
-                c->setPos( x()+5, y()-5);
-                c->tourelle->setPos( tourelle->x()+5, tourelle->y()-5);
-                c->centre.setx(cx+5);
-                c->centre.sety(cy-5);
-                break;
-            case 4:
-                c->setPos( x()+5, y());
-                c->tourelle->setPos( tourelle->x()+5, tourelle->y());
-                c->centre.setx(cx+5);
-                break;
-            case 5:
-                c->setPos( x()+5, y()+5);
-                c->tourelle->setPos( tourelle->x()+5, tourelle->y()+5);
-                c->centre.setx(cx+5);
-                c->centre.sety(cy+5);
-                break;
-            case 6:
-                c->setPos( x(), y()+5);
-                c->tourelle->setPos( tourelle->x(), tourelle->y()+5);
-                c->centre.sety(cy+5);
-                break;
-            case 7:
-                c->setPos( x()-5, y()+5);
-                c->tourelle->setPos( tourelle->x()-5, tourelle->y()+5);
-                c->centre.setx(cx-5);
-                c->centre.sety(cy+5);
-                break;
-            default:
-                break;
+                xp=-xp;
+                yp=-yp;
             }
-            pm--;
+
+            tourelle->setPos(tourelle->x()+xp, tourelle->y()+yp);
+
+            setPos( x()+xp, y()+yp);
+            centre.setx(cx+xp);
+            centre.sety(cy+yp);
+
+            if(c->peutBouger(tab))
+            {
+                cout<<centre.getx()<<endl;
+                cout<<"passage"<<endl;
+                //prendpos(c);
+                cout<<centre.getx()<<endl;
+            }
         }
-        if(c->peutBouger(tab))
+        else if(event->key() ==Qt::Key_Space || pm==0)
         {
-            cout<<centre.getx()<<endl;
-            cout<<"passage"<<endl;
-            prendpos(c);
-            cout<<centre.getx()<<endl;
+            clearFocus();
+            tourelle->setFocus();
         }
+        c->~Caisse();
     }
-    else if(event->key() ==Qt::Key_Space || pm==0)
-    {
-        clearFocus();
-        tourelle->setFocus();
-    }
-    c->~Caisse();
 }
 
-bool Caisse::peutBouger(Grilleterrain* tab)
-{
-    int x=centre.getx();
-    int y=centre.gety();
-    x-=-4;
-    y-=-4;
-    for(int i=0;i<9;i++)
+    bool Caisse::peutBouger(Grilleterrain* tab)
     {
-        for(int j=0;j<9;j++)
+        int x=centre.getx();
+        int y=centre.gety();
+        x-=-4;
+        y-=-4;
+        for(int i=0;i<9;i++)
         {
-            if(hitbox(i,j) && tab->est_traversable(i,j))
+            for(int j=0;j<9;j++)
             {
+                if(hitbox(i,j) && tab->est_traversable(i,j))
+                {
                     return false;
+                }
+
             }
-
         }
-    }
-    return true;
-}
-
-bool Caisse::hitbox(int x,int y)
-{
-    if(angle==0)
-    {
-        if(y>6){return false;}//dessus de la caisse
-        else if(y<2){return false;}//dessous de la caisse
-        else if(x==0&&(y==6||y==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
-        return true;
-    }
-    else if(angle==180)
-    {
-        if(y>6){return false;}//dessus de la caisse
-        else if(y<2){return false;}//dessous de la caisse
-        else if(x==8&&(y==6||y==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
-        return true;
-    }
-    else if(angle==90)
-    {
-        if(x>6){return false;}//dessus de la caisse
-        else if(x<2){return false;}//dessous de la caisse
-        else if(y==0&&(x==6||x==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
-        return true;
-    }
-    else if(angle==270)
-    {
-        if(x>6){return false;}//dessus de la caisse
-        else if(x<2){return false;}//dessous de la caisse
-        else if(y==8&&(x==6||x==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
-        return true;
-    }
-    else if(angle==45||angle==225)
-    {
-        int h45[9][9]=hitbox45d;
-
-        if(h45[x][y]==0){return false;}
-        return true;
-    }
-    else
-    {
-        int h135[9][9]=hitbox45d;
-
-        if(h135[9-y][x]==0){return false;}//rotation de la hitbox dans le sens inverse en changant le x et le y
         return true;
     }
 
-    exit (2);
-}
+    bool Caisse::hitbox(int x,int y)
+    {
+        if(angle==0)
+        {
+            if(y>6){return false;}//dessus de la caisse
+            else if(y<2){return false;}//dessous de la caisse
+            else if(x==0&&(y==6||y==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
+            return true;
+        }
+        else if(angle==180)
+        {
+            if(y>6){return false;}//dessus de la caisse
+            else if(y<2){return false;}//dessous de la caisse
+            else if(x==8&&(y==6||y==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
+            return true;
+        }
+        else if(angle==90)
+        {
+            if(x>6){return false;}//dessus de la caisse
+            else if(x<2){return false;}//dessous de la caisse
+            else if(y==0&&(x==6||x==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
+            return true;
+        }
+        else if(angle==270)
+        {
+            if(x>6){return false;}//dessus de la caisse
+            else if(x<2){return false;}//dessous de la caisse
+            else if(y==8&&(x==6||x==2)){return false;}//derière de la caisse qui n'est pas completement rectangulaire
+            return true;
+        }
+        else if(angle==45||angle==225)
+        {
+            int h45[9][9]=hitbox45d;
 
-void Caisse::prendpos(Caisse* c)
-{
-    this->angle=c->getAngle();
-    this->pm=c->getPm();
-    this->centre=c->getcentre();
-    this->tourelle=c->tourelle;
-    cout<<centre.getx()<<endl;
-}
+            if(h45[x][y]==0){return false;}
+            return true;
+        }
+        else
+        {
+            int h135[9][9]=hitbox45d;
+
+            if(h135[9-y][x]==0){return false;}//rotation de la hitbox dans le sens inverse en changant le x et le y
+            return true;
+        }
+
+        exit (2);
+    }
+
+    void Caisse::prendpos(Caisse* c)
+    {
+        this->angle=c->getAngle();
+        this->pm=c->getPm();
+        this->centre=c->getcentre();
+        this->tourelle=c->tourelle;
+        cout<<centre.getx()<<endl;
+    }
